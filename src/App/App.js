06d55d27b4fc661215ec4520a9163599ff91d495
cfloyd252 <cfloyd252@gmail.com>
2020-10-26 20:25:09 -0700

    started on landing page

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,11 @@
+import Landing from '../Pages/Landing/Landing';
+import { Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello</p>
+      <Route exact path={'/'} component={Landing} />
     </div>
   );
 }
---
+++ src/Pages/Landing/Landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome</h1>
+        <p>My name is Christopher Floyd and this is my submission for the Tamdem 2021 apprentice program coding challege.</p>
+      </div>
+    )
+  }
+}
+
+export default Landing
