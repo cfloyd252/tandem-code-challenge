f9e086831bd4380d7e943b2ab3fdbb9fd7a2fd43
cfloyd252 <cfloyd252@gmail.com>
2020-11-02 14:49:10 -0700

    Login.js using hooks

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,34 +1,36 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import TokenService from '../../Services/token-service';
 import AuthApiService from '../../Services/auth-api-service';
 
 import './Login.css'
 
 function Login(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleSubmitJwtAuth = async(ev) => {
     ev.preventDefault()
-    const { username, password } = ev.target
 
     try {
       const res = await AuthApiService.postLogin({
-        username: username.value,
-        password: password.value,
+        username,
+        password,
       })
 
       TokenService.saveAuthToken(res.authToken)
       props.history.push('/app/trivia')
     } catch(res) {
-      console.log(res.error)
+      console.log(res.error);
+      ev.target.password.value = '';
     }
   };
 
   return (
     <form id='loginForm' onSubmit={handleSubmitJwtAuth}>
       <label htmlFor='username' className='hidden'>Username</label>
-      <input id='username' type='text' placeholder='Username'/>
+      <input id='username' type='text' placeholder='Username' onChange={e => setUsername(e.target.value)}/>
       <label htmlFor='password' className='hidden'>Password</label>
-      <input id='password' type='password' placeholder='Password'/>
+      <input id='password' type='password' placeholder='Password' onChange={e => setPassword(e.target.value)}/>
       <button type='submit'>Submit</button>
     </form>
   )
