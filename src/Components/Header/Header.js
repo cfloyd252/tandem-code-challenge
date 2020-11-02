836a772d66ed0f18c12b5225c58f56de9c2cc395
cfloyd252 <cfloyd252@gmail.com>
2020-11-02 12:32:06 -0700

    changed app name

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ export class Header extends Component {
   render() {
     return (
       <header id='header'>
-        <Link className='headerLink' id='homeLink' to='/app/trivia'>Tandem Trivia</Link>
+        <Link className='headerLink' id='homeLink' to='/app/trivia'>Team Trivia</Link>
         <Link className='headerLink' id='leaderbaordLink' to='/app/leaderboard'>Leaderboard</Link>
         <Link className='headerLink' id='loginLink' to='/app/login'>Sign In</Link>
       </header>
