03a06691a55bb31c8e2028a4b203f047d3cf688d
cfloyd252 <cfloyd252@gmail.com>
2020-11-02 14:20:33 -0700

    cleaned up Login.js

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,36 +4,34 @@ import AuthApiService from '../../Services/auth-api-service';
 
 import './Login.css'
 
-export class Login extends Component {
+function Login(props) {
 
-  handleSubmitJwtAuth = ev => {
+  const handleSubmitJwtAuth = async(ev) => {
     ev.preventDefault()
     const { username, password } = ev.target
 
-    AuthApiService.postLogin({
-      username: username.value,
-      password: password.value,
-    })
-      .then(res => {
-        username.value = ''
-        password.value= ''
-        TokenService.saveAuthToken(res.authToken)
-        this.props.history.push('/app/trivia')
+    try {
+      const res = await AuthApiService.postLogin({
+        username: username.value,
+        password: password.value,
       })
-      .catch(res => this.context.setError(res.error))
-  }
 
-  render() {
-    return (
-      <form id='loginForm' onSubmit={this.handleSubmitJwtAuth}>
-        <label htmlFor='username' className='hidden'>Username</label>
-        <input id='username' type='text' placeholder='Username'/>
-        <label htmlFor='password' className='hidden'>Password</label>
-        <input id='password' type='password' placeholder='Password'/>
-        <button type='submit'>Submit</button>
-      </form>
-    )
-  }
+      TokenService.saveAuthToken(res.authToken)
+      props.history.push('/app/trivia')
+    } catch(res) {
+      console.log(res.error)
+    }
+  };
+
+  return (
+    <form id='loginForm' onSubmit={handleSubmitJwtAuth}>
+      <label htmlFor='username' className='hidden'>Username</label>
+      <input id='username' type='text' placeholder='Username'/>
+      <label htmlFor='password' className='hidden'>Password</label>
+      <input id='password' type='password' placeholder='Password'/>
+      <button type='submit'>Submit</button>
+    </form>
+  )
 }
 
 export default Login
