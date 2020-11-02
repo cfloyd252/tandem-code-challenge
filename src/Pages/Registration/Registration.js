d33a2cb330d2201d9a88cd5a1fc776906704d1f1
cfloyd252 <cfloyd252@gmail.com>
2020-11-02 15:45:23 -0700

    able to add new user

--- src/Pages/Registration/Registration.js
+++ src/Pages/Registration/Registration.js
@@ -1,21 +1,53 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import UsersApiService from '../../Services/user-api-service';
+import AuthApiService from '../../Services/auth-api-service';
+import TokenService from '../../Services/token-service';
 
-function Registration() {
+
+function Registration(props) {
   const [firstName, setFirstName] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmation, setConfirmation] = useState('');
+
+  const handleSubmit = (ev) => {
+    ev.preventDefault()
+
+    const newUser = {
+      first_name: firstName,
+      username,
+      password
+    }
+
+    UsersApiService.postUser(newUser)
+    .then(user => {
+      // this.context.setUser(user)
+      AuthApiService.postLogin({
+        username,
+        password
+      })
+        .then(res => {
+          TokenService.saveAuthToken(res.authToken)
+          props.history.push('/app/trivia')
+        })
+    })
+    .catch(res => {
+      // this.context.setError(res.error)
+      console.log(res.error)
+    });
+  }
 
   return (
-    <form id='registrationForm'>
+    <form id='registrationForm' onSubmit={handleSubmit}>
       <h1>Registration</h1>
       <label className='hidden' htmlFor='firstName'>First Name</label>
-      <input id='firstName' placeholder='First Name' />
+      <input id='firstName' placeholder='First Name' onChange={e => setFirstName(e.target.value)}/>
       <label className='hidden' htmlFor='username'>Create Username</label>
-      <input id='username' placeholder='Create Username'/>
+      <input id='username' placeholder='Create Username' onChange={e => setUsername(e.target.value)}/>
       <label className='hidden' htmlFor='password'>Create Password</label>
-      <input id='password' type='password' placeholder='Create Password'/>
+      <input id='password' type='password' placeholder='Create Password' onChange={e => setPassword(e.target.value)}/>
       <label className='hidden' htmlFor='passwordConfirmation'>Confirm Password</label>
-      <input id='passwordConfirmation' type='password' placeholder='Confirm Password'/>
+      <input id='passwordConfirmation' type='password' placeholder='Confirm Password' onChange={e => setConfirmation(e.target.value)}/>
       <button type='submit'>Submit</button>
     </form>
   )
---
+++ src/Services/user-api-service.js
@@ -0,0 +1,33 @@
+import config from '../config'
+import TokenService from '../Services/token-service'
+
+const UserApiService = {
+  getUser() {
+    return fetch(`${config.API_ENDPOINT}/user`, {
+      headers: {
+        'authorization': `bearer ${TokenService.getAuthToken()}`,
+      },
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+  postUser(user) {
+    return fetch(`${config.API_ENDPOINT}/user`, {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+      .then(res =>
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      )
+  },
+}
+
+export default UserApiService;
