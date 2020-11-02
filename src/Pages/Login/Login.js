34609bbb4a2abe0241fd24ea48077d6697fe9ae3
cfloyd252 <cfloyd252@gmail.com>
2020-11-02 13:51:40 -0700

    login working

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TokenService from '../../Services/token-service';
+import AuthApiService from '../../Services/auth-api-service';
+
+import './Login.css'
 
 export class Login extends Component {
+
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    const { username, password } = ev.target
+
+    AuthApiService.postLogin({
+      username: username.value,
+      password: password.value,
+    })
+      .then(res => {
+        username.value = ''
+        password.value= ''
+        TokenService.saveAuthToken(res.authToken)
+        this.props.history.push('/app/trivia')
+      })
+      .catch(res => this.context.setError(res.error))
+  }
+
   render() {
     return (
-      <form id='loginForm'>
+      <form id='loginForm' onSubmit={this.handleSubmitJwtAuth}>
         <label htmlFor='username' className='hidden'>Username</label>
         <input id='username' type='text' placeholder='Username'/>
         <label htmlFor='password' className='hidden'>Password</label>
         <input id='password' type='password' placeholder='Password'/>
-        <button>Submit</button>
+        <button type='submit'>Submit</button>
       </form>
     )
   }
