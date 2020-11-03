d433365835b8d8cc8a31577ef84873cde58686bd
cfloyd252 <cfloyd252@gmail.com>
2020-11-03 01:22:57 -0700

    render logout button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import TokenService from '../../Services/token-service'
 import './Header.css'
 
-export class Header extends Component {
-  render() {
+function Header(props) {
+  const handleLogOut = () => {
+    TokenService.clearAuthToken()
+    props.history.push('/app/login')
+  }
+
+  const renderAuthorization = () => {
     return (
-      <header id='header'>
-        <Link className='headerLink' id='homeLink' to='/app/trivia'>Team Trivia</Link>
-        <Link className='headerLink' id='leaderbaordLink' to='/app/leaderboard'>Leaderboard</Link>
-        <Link className='headerLink' id='loginLink' to='/app/login'>Sign In</Link>
-      </header>
+      TokenService.hasAuthToken()
+        ? <button onClick={handleLogOut}>Log Out</button>
+        : <Link className='headerLink' id='loginLink' to='/app/login'>Sign In</Link>
     )
   }
+
+  return (
+    <header id='header'>
+      <Link className='headerLink' id='homeLink' to='/app/trivia'>Team Trivia</Link>
+      <Link className='headerLink' id='leaderbaordLink' to='/app/leaderboard'>Leaderboard</Link>
+      {renderAuthorization()}
+    </header>
+  )
 }
 
 export default Header
